722a9b98856ecd5c2c1542c5457c7a7d6726c576
Mattion <omarturkmah@gmail.com>
2023-09-21 01:38:26 +0300

    I think I added an evn that should function properly? not fixing issue#1 because I legit don't know if I fixed anything

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 from sys import argv
 import json
+import os
 
+from dotenv import load_dotenv
 from bot import client
 
+load_dotenv("../.env")
 if len(argv) < 2:
-    raise Exception('No config provided')
+    raise Exception('No config provided') 
+   
 
 config_path = argv[1]
 config = json.load(open(config_path, "r"))
 
 if 'token' not in config:
-    raise Exception('No token')
-
-token = config['token']
+    #raise Exception('No token') isn't the point of this is that the program keeps running even after running into a problem?
+    token = os.environ.get('BOT_TOKEN')
+else:
+    token = config['token']
 
 client.run(token)

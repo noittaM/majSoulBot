7c90f9b8f6f47246ac2cfbc12dee5c370ed54d8b
Mattion <omarturkmah@gmail.com>
2023-09-21 20:26:07 +0300

    figuring out how to work with events, broken state doesn't function. will fix later

--- src/bot.py
+++ src/bot.py
@@ -10,11 +10,30 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'logged in as {client.user}')
 
+@client.event
+async def evalute(message):
+    await message.channel.send('type in the first number')
+    if message.content.startswith('$'):
+        number1=message.content
+        await message.channel.send('type in the second number')
+        if message.content.startswith('$'):
+            number2=message.content
+    message.channel.send("the first number is " + number1 + "and the second is "+ number2)
+    
+
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author.bot:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('$eval'):
+        await evalute(message)
+
+
+
+
